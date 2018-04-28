be370fe1c6941764f6b5b8280f7c1ffbe30ee2ea
Kate <je4282oi@go.minneapolis.edu>
2018-04-28 15:49:54 -0500

    First commit

---
+++ src/com/Kate/Main.java
@@ -0,0 +1,10 @@
+package com.Kate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is the project so far... ");
+        System.out.println("But it's still not there");
+    }
+}
